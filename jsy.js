eb66b69b41fc637529143020074ac6fac01c84d7
Shane Holloway <shane.holloway@ieee.org>
2023-01-24 16:15:04 -0700

    Moved node-loader logic from jsy-transpile into this package

--- jsy.js
+++ jsy.js
@@ -1,3 +1,25 @@
-export {resolve, load, jsy_transpile_srcmap} from 'jsy-transpile/esm/node-loader.mjs'
-export {eleventy_jsy} from './eleventy_jsy.js'
+//import { jsy_transpile_srcmap } from '@jsy-lang/jsy'
+import { jsy_transpile_srcmap } from 'jsy-transpile'
+export { eleventy_jsy } from './eleventy_jsy.js'
 
+export { jsy_transpile_srcmap }
+
+const jsy_options = { sourcemap: 'inline', as_rec: false }
+
+export async function load(url, context, nextLoad) {
+  if (! /\.jsy$/.test(new URL(url).pathname))
+    return nextLoad(url, context, nextLoad)
+
+  let format = 'module' // use module format to load raw source
+  let {source: raw_jsy} = await nextLoad(url, { ...context, format })
+  raw_jsy = raw_jsy.toString('utf-8')
+
+  let source = jsy_transpile_srcmap(raw_jsy, url, jsy_options)
+  return { format, source }
+}
+
+
+export async function resolve(specifier, context, nextResolve) {
+  // as .jsy are local real files, we don't need a custom resolver
+  return nextResolve(specifier, context)
+}
